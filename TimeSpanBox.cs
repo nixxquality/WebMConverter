8b316deecbc64cff948c277337014e104cbebd67
nixx quality <nixx@is-fantabulo.us>
2014-06-08 02:38:32 +0200

    TimeSpanBox fully featured

--- TimeSpanBox.cs
+++ TimeSpanBox.cs
@@ -7,16 +7,56 @@ namespace WebMConverter
 {
     public partial class TimeSpanBox : TextBox
     {
+        const string Format = @"hh\:mm\:ss\.fff";
+        const int FormatLength = 12;
+
         public TimeSpan Value
         {
             get
             {
-                return TimeSpan.ParseExact(Text, @"hh\:mm\:ss\.fff", CultureInfo.CurrentCulture);
+                return TimeSpan.ParseExact(Text, Format, CultureInfo.CurrentCulture);
             }
             set
             {
-                Text = value.ToString(@"hh\:mm\:ss\.fff");
+                Text = value.ToString(Format);
             }
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+
+            int index = SelectionStart;
+
+            if (char.IsDigit(e.KeyChar) && index != FormatLength)
+            {
+                if (!char.IsDigit(Text[index]))
+                {
+                    index++;
+                }
+
+                Text = Text.Remove(index, 1).Insert(index, e.KeyChar.ToString());
+
+                index++;
+            }
+            else if (e.KeyChar == '\b' && index != 0) // Backspace
+            {
+                index--;
+
+                if (!char.IsDigit(Text[index]))
+                {
+                    index--;
+                }
+
+                if (char.IsDigit(Text[index]))
+                {
+                    Text = Text.Remove(index, 1).Insert(index, "0");
+                }
+            }
+
+            SelectionStart = Math.Max(0, Math.Min(index, FormatLength));
+
+            e.Handled = true;
         }
     }
 }
