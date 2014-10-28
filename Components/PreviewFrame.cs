5b9cb89ecfb0b458b154281b7cee2efb84f6a090
nixx quality <nixx@is-fantabulo.us>
2014-10-28 01:54:31 +0100

    You're still doing it!

--- Components/PreviewFrame.cs
+++ Components/PreviewFrame.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace WebMConverter
@@ -22,16 +24,19 @@ namespace WebMConverter
         public PreviewFrame()
         {
             InitializeComponent();
+
+            // Prepare our "list" of accepted pixel formats
+            List<int> pixelformat = new List<int>();
+            pixelformat.Add(FFMSSharp.FFMS2.GetPixelFormat("bgra"));
+
+            var infoframe = Program.VideoSource.GetFrame((int)framenumber);
+            Program.VideoSource.SetOutputFormat(pixelformat, infoframe.EncodedResolution.Width, infoframe.EncodedResolution.Height, FFMSSharp.Resizer.Bilinear);
         }
 
         public void GeneratePreview()
         {
             if (Program.VideoSource == null)
                 return;
-
-            // Prepare our "list" of accepted pixel formats
-            List<int> pixelformat = new List<int>();
-            pixelformat.Add(FFMSSharp.FFMS2.GetPixelFormat("bgra"));
 
             // Load the frame, if we haven't already
             if (cachedframenumber != framenumber)
@@ -41,27 +46,44 @@ namespace WebMConverter
             }
 
             // Calculate width and height
-            int w, h;
-            float s;
-            s = Math.Min((float)this.Size.Width / (float)frame.EncodedResolution.Width, (float)this.Size.Height / (float)frame.EncodedResolution.Height);
-            w = (int)(frame.EncodedResolution.Width * s);
-            h = (int)(frame.EncodedResolution.Height * s);
+            int width, height;
+            float scale;
+            scale = Math.Min((float)this.Size.Width / (float)frame.EncodedResolution.Width, (float)this.Size.Height / (float)frame.EncodedResolution.Height);
+            width = (int)(frame.EncodedResolution.Width * scale);
+            height = (int)(frame.EncodedResolution.Height * scale);
 
-            // Do all the work
-            Program.VideoSource.SetOutputFormat(pixelformat, w, h, FFMSSharp.Resizer.Bilinear);
-            frame = Program.VideoSource.GetFrame((int)this.framenumber);
+            // http://stackoverflow.com/a/24199315/174466
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
 
-            Picture.BackgroundImage = frame.Bitmap;
-            Picture.ClientSize = new Size(w, h);
+            destImage.SetResolution(frame.EncodedResolution.Width, frame.EncodedResolution.Height);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(frame.Bitmap, destRect, 0, 0, frame.EncodedResolution.Width, frame.EncodedResolution.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            Picture.BackgroundImage = destImage;
+            Picture.ClientSize = new Size(width, height);
 
             // Center the pictureBox in our control
-            if (w == Width || w - 1 == Width || w + 1 == Width) // this looks weird but keep in mind we're dealing with an ex float here
+            if (width == Width || width - 1 == Width || width + 1 == Width) // this looks weird but keep in mind we're dealing with an ex float here
             {
-                Padding = new Padding(0, (Height - h) / 2, 0, 0);
+                Padding = new Padding(0, (Height - height) / 2, 0, 0);
             }
             else
             {
-                Padding = new Padding((Width - w) / 2, 0, 0, 0);
+                Padding = new Padding((Width - width) / 2, 0, 0, 0);
             }
         }
 
